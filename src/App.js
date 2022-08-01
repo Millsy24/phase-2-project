452b77471ab444a9d57ccb8d15755c348346a3ad
Jonathon <jwbialas@gmail.com>
2022-08-01 16:14:01 -0500

    cryptid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import TrackPage from './Components/TrackPage'
 import SubmitPage from './Components/SubmitPage'
@@ -6,6 +6,9 @@ import CryptidsPage from './Components/CryptidsPage'
 import NavBar from './Components/NavBar'
 
 function App() {
+  useEffect(()=>{
+    fetch('local')
+  })
   return (
     <div className="App">
      <NavBar/>
