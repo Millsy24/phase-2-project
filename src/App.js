436464e7e31df2b81ebbfd8b6082ffb382cfb288
Jonathon <jwbialas@gmail.com>
2022-08-01 16:25:41 -0500

    NavBar needs work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react'
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 import './App.css'
 import TrackPage from './Components/TrackPage'
 import SubmitPage from './Components/SubmitPage'
@@ -19,5 +21,11 @@ function App() {
     </div>
   );
 }
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+  </BrowserRouter>,
+  document.getElementById("root")
+  );
 
 export default App;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -13,4 +13,4 @@ const NavBar = () => {
 
 
 
-export default NavBar
+export default NavBar;
