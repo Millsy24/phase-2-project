47df6761da21452a75473b8193680ccdcd901e0a
Lukas Mills <lukascodes64@gmail.com>
2022-08-01 16:46:10 -0400

    created components

---
+++ src/Components/TrackCardContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TrackCard from './TrackCard'
+
+
+
+
+
+const TrackCardContainer = () => {
+
+
+
+
+return (
+    <div className ="card container">
+        <TrackCard/>
+    </div>
+)
+
+
+}
+
+
+
+export default TrackCardContainer
