8935fa9fe3ee79e621d336fe71460e27274fb8c2
Alex <friedmanalexg@gmail.com>
2022-08-02 16:23:44 -0400

    trackcard imported some lost code

--- src/Components/TrackCard.js
+++ src/Components/TrackCard.js
@@ -3,13 +3,17 @@ import React from 'react'
 
 
 
-const TrackCard = ({ user, date, body, image, location}) => {
-return(
-    <div >
-
-    </div>
-)}
-
-
+const TrackCard = ({ id, user, date, body, image, location}) => {
+    return(
+        <div id = {id}>
+            <p>{user}</p>
+            <p>{date}</p>
+            <p>{body}</p>
+            <p>{location}</p>
+            <img src ={image} alt = "monster that tells the future"/>
+    
+        </div>
+    )}
+    
 
 export default TrackCard
